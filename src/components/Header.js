2d70d12db3d389712cb1c4692c2b2006e51c3c4e
Rodrigo <cherry199662@gmail.com>
2019-01-14 09:42:53 -0500

    Init Project

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class Header extends Component {
+
+  render() {
+
+    return (
+
+      <div className="App-header">
+        <p>this is the header.</p>
+      </div>
+
+    )
+
+  }
+
+}
+
+export default Header;
